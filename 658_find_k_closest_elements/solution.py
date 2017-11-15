bfc10526754c9cea5c081dbbc08ac827e233b9b0
Leo Du <dul2@uw.edu>
2017-11-15 10:16:07 -0800

    initial commit

---
+++ 658_find_k_closest_elements/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        
