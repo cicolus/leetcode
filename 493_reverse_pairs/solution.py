bf4106d8faafb31939a5271fbb61e5f623dc567b
Leo Du <dul2@uw.edu>
2017-11-26 00:31:37 -0800

    todo

--- 493_reverse_pairs/solution.py
+++ 493_reverse_pairs/solution.py
@@ -7,3 +7,15 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        # TODO: implement divide and conquer solution
+
+    def reverse_pairs_dc(self, nums, i, j):
+        """
+        :type nums: List[int]
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        if i >= j:
+            return 0
+        mid = (i+j) // 2
