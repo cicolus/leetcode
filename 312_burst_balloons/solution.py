7213b0cf6d6399e7509b8a86a870d32930840858
Leo Du <dul2@uw.edu>
2017-12-03 18:27:17 -0800

    dynamic programming with correct recurrence, tests passed

--- 312_burst_balloons/solution.py
+++ 312_burst_balloons/solution.py
@@ -1,4 +1,5 @@
 from typing import List
+from pprint import pprint
 from math import inf
 
 class Solution:
@@ -7,38 +8,21 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        nums = [1] + nums + [1]
         size = len(nums)
-        max_coin = -inf
         dp = [[0] * size for _ in range(size)]
-        for diff in range(size):
-            for i in range(size - diff):
-                if diff == 0:
-                    dp[i][i] = nums[i]
-                else:
-                    temp = [nums[i] * nums[i + 1] + dp[i + 1][i + diff]]
-                    temp.append(nums[i + diff - 1] * nums[i + diff] + dp[i][i + diff - 1])
-                    for k in range(i + 1, i + diff):
-                        coin = nums[k] * nums[k - 1] * nums[k + 1] + dp[i][k - 1] + dp[k + 1][i + diff]
-                        if k + 1 != i + diff and k - 1 != i:
-                            coin -= nums[k + 1] * nums[k + 2] + nums[k - 1] * nums[k - 2]
-                            coin += nums[k - 1] * nums[k + 1] * nums[k + 2] + nums[k - 2] * nums[k - 1] * nums[k + 1]
-                        elif k + 1 != i + diff:
-                            coin -= nums[k + 1] * nums[k + 2]
-                            coin += nums[k - 1] * nums[k + 1] * nums[k + 2]
-                        elif k - 1 != i:
-                            coin -= nums[k - 1] * nums[k - 2]
-                            coin += nums[k - 2] * nums[k - 1] * nums[k + 1]
-                        else:
-                            coin -= nums[k - 1] + nums[k + 1]
-                            coin += nums[k - 1] * nums[k + 1]
-                        temp.append(coin)
-                    curr = max(temp)
-                    dp[i][i + diff] = curr
-                    if diff == size - 1:
-                        max_coin = max(curr, max_coin)
-        print(dp)
-        return max_coin
+        for diff in range(size - 2):
+            for i in range(1, size - diff - 1):
+                j = i + diff
+                for k in range(i, j + 1):
+                    dp[i][j] = max(dp[i][j], dp[i][k - 1] + nums[k] * nums[i- 1] * nums[j + 1] + dp[k + 1][j])
+        return dp[1][size - 2]
+
 
 if __name__ == '__main__':
     test = Solution()
-    print(test.maxCoins([3, 1, 5, 8]))
+    print(test.maxCoins([3, 1, 5, 8])) # 167
+    print(test.maxCoins([])) # 0
+    print(test.maxCoins([1, 2, 3, 4, 5, 6, 7, 8])) # 912
+    print(test.maxCoins([1, 2, 3, 4, 5, 6, 7, 8, 9])) # 1530
+    print(test.maxCoins([0, 1, 2, 3, 4, 5, 6, 7, 8])) # 912
